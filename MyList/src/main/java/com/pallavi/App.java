1714113772d589f0f5644861942a4e3b84a50dbd
pallavisai23 <54880008+pallavisai23@users.noreply.github.com>
2020-03-05 15:58:31 +0530

    Add files via upload

---
+++ MyList/src/main/java/com/pallavi/App.java
@@ -0,0 +1,18 @@
+package com.pallavi;
+/**
+ * Hello world!
+ *
+ */
+import org.apache.logging.log4j.*;
+public class App {
+
+	private static final Logger logger = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	Integer[] array= {23,45};
+    	MyList<Integer> list = new MyList<Integer>(array);
+    	list.add(23);
+    	logger.debug(list);
+    }
+
+}
